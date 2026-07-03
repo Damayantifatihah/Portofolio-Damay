f1fdc9246cb1d51c46f35c618f6d79dee43e5dfc
Damayantifatihah <fatihahdamay2@gmail.com>
2026-07-03 18:49:41 +0700

    language change

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink, Link, useLocation } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 interface NavItem {
